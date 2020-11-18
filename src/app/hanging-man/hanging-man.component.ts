2ce5d7b19270a645f4c8776247a9ac53c8075b32
Mandy Blaschke <post@mandy-blaschke.de>
2020-11-18 12:50:26 +0100

    added: new component, styles and first animations for hanging-man

---
+++ src/app/hanging-man/hanging-man.component.ts
@@ -0,0 +1,19 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-hanging-man',
+  templateUrl: './hanging-man.component.html',
+  styleUrls: ['./hanging-man.component.scss']
+})
+export class HangingManComponent implements OnInit {
+
+
+  @Input() faults: number;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
